ea8d1f3c8c59830da6fff6b5f2f6dd612ac31af2
davidcoats <david@getsafetycone.com>
2022-06-09 12:43:32 -0700

    Interim changes.

---
+++ source/R5T.X0015/Code/Instances.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.X0015
+{
+    public static class Instances
+    {
+        public static B0006.ISyntaxGenerator SyntaxGenerator { get; } = B0006.SyntaxGenerator.Instance;
+        public static B0007.ISpacingGenerator SpacingGenerator { get; } = B0007.SpacingGenerator.Instance;
+    }
+}
--- source/R5T.X0015/Code/Interfaces/Interface1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-
-namespace R5T.X0015
-{
-    public interface Interface1
-    {
-    }
-}
